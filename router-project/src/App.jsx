91230a61f84651f5497cbf7ebf027e1fdd7e6dae
Falak-Chudasama <falakchudasama7766@gmail.com>
2025-03-09 08:56:38 +0530

    Initial Commit for Router Project

---
+++ router-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function App() {
+    return(
+        <>
+            <h1 className='text-9xl font-extrabold text-center'>Router Project</h1>
+        </>
+    );
+}
+
+export default App;
