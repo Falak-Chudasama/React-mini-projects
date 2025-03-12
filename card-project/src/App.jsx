4df9ed289e0ca7d46624a5f8f85c3bbac4e2bb52
Falak-Chudasama <falakchudasama7766@gmail.com>
2025-03-12 05:57:22 +0530

    add theme context and toggle functionality; update components for theme support

--- card-project/src/App.jsx
+++ card-project/src/App.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './components/Card.jsx';
 import ThemeBtn from './components/ThemeBtn.jsx';
+import { ThemeContextProvider, useTheme } from './context/themeContext.js';
 
 function App() {
+    const [themeMode, setThemeMode] = useState('light');
+
+    const lightMode = () => {
+        setThemeMode('light');
+    }
+
+    const darkMode = () => {
+        setThemeMode('dark');
+    }
+
+    useEffect(() => {
+        document.querySelector('html').classList.remove('light', 'dark');
+        document.querySelector('html').classList.add(themeMode);
+    }, [themeMode]);
+
     return (
-        <div className="flex flex-wrap min-h-screen items-center">
-            <div className="w-full">
-                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+        <ThemeContextProvider value={{ themeMode, lightMode, darkMode }}>
+            <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <ThemeBtn></ThemeBtn>
+                    </div>
 
-                </div>
-
-                <div className="w-full max-w-sm mx-auto">
-
+                    <div className="w-full max-w-sm mx-auto">
+                        <Card></Card>
+                    </div>
                 </div>
             </div>
-        </div>
+        </ThemeContextProvider>
     );
 }
 
